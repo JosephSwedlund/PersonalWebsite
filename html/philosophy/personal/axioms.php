9a89dba31eccc24f58a30c27e87070b0dd3e695c
Joseph Swedlind <JosephSwedlund@gmail.com>
2022-03-23 00:49:59 -0500

    adding the beginnings of the philosophical aspect to my website.

---
+++ html/philosophy/personal/axioms.php
@@ -0,0 +1,45 @@
+
+This page is a list of things I call axioms. Axioms can exist in on of three states. They are either: 1: A pure assumption. 2: Something which is claimed to be self evident. or 3: Something which has been logically concluded from other axioms
+this third type of axiom only seperates itself from other philosophical conclusions later on as these conclusions do not directly make a claim about Epistemology, Ethics, Metaphysics, Aethetics, or Ontology. While the claims made in those sections of my writting
+will be based upon the building blocks here. Because these tertiary axioms which are deduced from the primary and secondary axioms but do not directly imply something about those fields of philosophy they are still considered axioms and not philosophical statements of a given domain themselves.
+
+
+But before I get into the axioms I will here have a list of (ideally) useful definitions as I am using them.
+1: "True Nature" |  The (seeming) underlying characteristics of a given object or phenomonon
+
+2: "Understand/Understanding" | To have understanding of something is to have some form of insite into its true nature. 
+
+3: "The World" | The thing which seems to lie  beyond our self we seem to have an inuitive belief in its existence  and we seem to be capible of effecting and being effected by it.
+
+
+Primary Axioms (Pure Assumptions)
+1: It is better to have fewer and more simple axioms when attempting to create a worldview as this decreases the likelyhood of making unreasonable assumptions.
+
+2: It is possible to understand the outside world to at least some extent.
+
+3: Language is a useful means by which to communicate about what is understood.
+
+4: observation preceeds logic. 
+
+
+Secondary Axioms (Self evident claims)
+1: There is a thing which I call self.
+
+2: This self is capible of will.
+
+3: There exist something which we denote as "The World"
+
+4: It is possible to understand at least something (that thoughts exist) 
+
+5: to discuss something meaningfully we must be able to understand the aspect of it which we are discussing at least to some degree.
+
+6: To discuss something meaningfully we must be able to distinguish it from other.
+
+7: The only extent which we can understand something which doesn't exist is to understand it doesn't exist. To discuss the unreal beyond that is meaningless. 
+
+8: The world is at least to some degree predictible and ordered. 
+
+
+Tertiary Axioms (logical conclusions or extractions from primary and or secondary axioms)
+
+1: following P1 and S8 we can conclude that the logical 
