89a1d154999619d50919012caa9b8b2a906d0843
Joseph Swedlind <JosephSwedlund@gmail.com>
2022-03-23 22:07:50 -0500

    added some more layout changes

--- html/philosophy/personal/axioms.php
+++ html/philosophy/personal/axioms.php
@@ -11,47 +11,48 @@ will be based upon the building blocks here. Because these tertiary axioms which
 But before I get into the axioms I will here have a list of (ideally) useful definitions as I am using them.
 </p>
 <ul>
-<li>1: "True Nature" |  The (seeming) underlying characteristics of a given object or phenomonon
+<li>D1: "True Nature" |  The (seeming) underlying characteristics of a given object or phenomonon
 
-<li>2: "Understand/Understanding" | To have understanding of something is to have some form of insite into its true nature. 
+<li>D2: "Understand/Understanding" | To have understanding of something is to have some form of insite into its true nature. 
 
-<li>3: "The World" | The thing which seems to lie  beyond our self we seem to have an inuitive belief in its existence  and we seem to be capible of effecting and being effected by it.
+<li>D3: "The World" | The thing which seems to lie  beyond our self we seem to have an inuitive belief in its existence  and we seem to be capible of effecting and being effected by it.
 </ul>
 
 <h3>Primary Axioms (Pure Assumptions)</h3>
 <ul>
-<li>1: It is better to have fewer and more simple axioms when attempting to create a worldview as this decreases the likelyhood of making unreasonable assumptions.
+<li>P1: It is better to have fewer and more simple axioms when attempting to create a worldview as this decreases the likelyhood of making unreasonable assumptions.
 
-<li>2: It is possible to understand the outside world to at least some extent.
+<li>P2: It is possible to understand the outside world to at least some extent.
 
-<li>3: Language is a useful means by which to communicate about what is understood.
+<li>P3: Language is a useful means by which to communicate about what is understood.
 
-<li>4: observation preceeds logic. 
+<li>P4: observation preceeds logic. 
 </ul>
 
 <h3> Secondary Axioms (Self evident claims)</h3>
 <ul>
-<li>1: There is a thing which I call self.
+<li>S1: There is a thing which I call self.
 
-<li>2: This self is capible of will.
+<li>S2: This self is capible of will.
 
-<li>3: There exist something which we denote as "The World"
+<li>S3: There exist something which we denote as "The World"
 
-<li>4: It is possible to understand at least something (that thoughts exist) 
+<li>S4: It is possible to understand at least something (that thoughts exist) 
 
-<li>5: to discuss something meaningfully we must be able to understand the aspect of it which we are discussing at least to some degree.
+<li>S5: to discuss something meaningfully we must be able to understand the aspect of it which we are discussing at least to some degree.
 
-<li>6: To discuss something meaningfully we must be able to distinguish it from other.
+<li>S6: To discuss something meaningfully we must be able to distinguish it from other.
 
-<li>7: The only extent which we can understand something which doesn't exist is to understand it doesn't exist. To discuss the unreal beyond that is meaningless. 
+<li>S7: The only extent which we can understand something which doesn't exist is to understand it doesn't exist. To discuss the unreal beyond that is meaningless. 
 
-<li>8: The world is at least to some degree predictible and ordered. 
+<li>S8: The world is at least to some degree predictible and ordered. 
 </ul>
 
 <h3>Tertiary Axioms (logical conclusions or extractions from primary and or secondary axioms)</h3>
 
-1: following P1 and S8 we can conclude that the logical 
-
+<ul>
+<li>T1: following P1 and S8 we can conclude that the logical 
+</ul>
 
 
 
