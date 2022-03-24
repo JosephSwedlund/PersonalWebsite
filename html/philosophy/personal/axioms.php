58ed66dab7949d1ad0ffb422f1c40987db750659
Joseph Swedlind <JosephSwedlund@gmail.com>
2022-03-23 22:00:14 -0500

    formatting changes

--- html/philosophy/personal/axioms.php
+++ html/philosophy/personal/axioms.php
@@ -1,45 +1,58 @@
 
+<body>
+
+
+<h4>
 This page is a list of things I call axioms. Axioms can exist in on of three states. They are either: 1: A pure assumption. 2: Something which is claimed to be self evident. or 3: Something which has been logically concluded from other axioms
 this third type of axiom only seperates itself from other philosophical conclusions later on as these conclusions do not directly make a claim about Epistemology, Ethics, Metaphysics, Aethetics, or Ontology. While the claims made in those sections of my writting
 will be based upon the building blocks here. Because these tertiary axioms which are deduced from the primary and secondary axioms but do not directly imply something about those fields of philosophy they are still considered axioms and not philosophical statements of a given domain themselves.
-
-
+</h4>
+<p>
 But before I get into the axioms I will here have a list of (ideally) useful definitions as I am using them.
-1: "True Nature" |  The (seeming) underlying characteristics of a given object or phenomonon
+</p>
+<ul>
+<li>1: "True Nature" |  The (seeming) underlying characteristics of a given object or phenomonon
 
-2: "Understand/Understanding" | To have understanding of something is to have some form of insite into its true nature. 
+<li>2: "Understand/Understanding" | To have understanding of something is to have some form of insite into its true nature. 
 
-3: "The World" | The thing which seems to lie  beyond our self we seem to have an inuitive belief in its existence  and we seem to be capible of effecting and being effected by it.
+<li>3: "The World" | The thing which seems to lie  beyond our self we seem to have an inuitive belief in its existence  and we seem to be capible of effecting and being effected by it.
+</ul>
 
+<h3>Primary Axioms (Pure Assumptions)</h3>
+<ul>
+<li>1: It is better to have fewer and more simple axioms when attempting to create a worldview as this decreases the likelyhood of making unreasonable assumptions.
 
-Primary Axioms (Pure Assumptions)
-1: It is better to have fewer and more simple axioms when attempting to create a worldview as this decreases the likelyhood of making unreasonable assumptions.
+<li>2: It is possible to understand the outside world to at least some extent.
 
-2: It is possible to understand the outside world to at least some extent.
+<li>3: Language is a useful means by which to communicate about what is understood.
 
-3: Language is a useful means by which to communicate about what is understood.
+<li>4: observation preceeds logic. 
+</ul>
 
-4: observation preceeds logic. 
+<h3> Secondary Axioms (Self evident claims)</h3>
+<ul>
+<li>1: There is a thing which I call self.
 
+<li>2: This self is capible of will.
 
-Secondary Axioms (Self evident claims)
-1: There is a thing which I call self.
+<li>3: There exist something which we denote as "The World"
 
-2: This self is capible of will.
+<li>4: It is possible to understand at least something (that thoughts exist) 
 
-3: There exist something which we denote as "The World"
+<li>5: to discuss something meaningfully we must be able to understand the aspect of it which we are discussing at least to some degree.
 
-4: It is possible to understand at least something (that thoughts exist) 
+<li>6: To discuss something meaningfully we must be able to distinguish it from other.
 
-5: to discuss something meaningfully we must be able to understand the aspect of it which we are discussing at least to some degree.
+<li>7: The only extent which we can understand something which doesn't exist is to understand it doesn't exist. To discuss the unreal beyond that is meaningless. 
 
-6: To discuss something meaningfully we must be able to distinguish it from other.
+<li>8: The world is at least to some degree predictible and ordered. 
+</ul>
 
-7: The only extent which we can understand something which doesn't exist is to understand it doesn't exist. To discuss the unreal beyond that is meaningless. 
-
-8: The world is at least to some degree predictible and ordered. 
-
-
-Tertiary Axioms (logical conclusions or extractions from primary and or secondary axioms)
+<h3>Tertiary Axioms (logical conclusions or extractions from primary and or secondary axioms)</h3>
 
 1: following P1 and S8 we can conclude that the logical 
+
+
+
+
+</body>
